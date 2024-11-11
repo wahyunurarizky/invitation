b21cbc673112f4db952494463f6d0c4e6e3df7f8
wahyu <arizky.wna@gmail.com>
2024-11-11 09:46:02 +0800

    force fs

--- src/MobileView.tsx
+++ src/MobileView.tsx
@@ -13,6 +13,20 @@ const MobileView = ({ showMusicAndPlay }: { showMusicAndPlay: () => void }) => {
   const handleOpenInvitation = () => {
     setIsCoverVisible(false);
     showMusicAndPlay();
+
+    // Request fullscreen
+    if (document.documentElement.requestFullscreen) {
+      document.documentElement.requestFullscreen();
+    } else if (document.documentElement.mozRequestFullScreen) {
+      // Firefox
+      document.documentElement.mozRequestFullScreen();
+    } else if (document.documentElement.webkitRequestFullscreen) {
+      // Chrome, Safari and Opera
+      document.documentElement.webkitRequestFullscreen();
+    } else if (document.documentElement.msRequestFullscreen) {
+      // IE/Edge
+      document.documentElement.msRequestFullscreen();
+    }
   };
 
   useEffect(() => {
